b02635c2759920495438d70e538af7376be6d552
Tetsuya <tetsuya.morizumi@gmail.com>
2014-01-25 22:03:35 +0900

    misc.

--- tinet/netinet/ip_icmp.c
+++ tinet/netinet/ip_icmp.c
@@ -4,7 +4,7 @@
  *  Copyright (C) 2001-2009 by Dep. of Computer Science and Engineering
  *                   Tomakomai National College of Technology, JAPAN
  *
- *  上記著作権者は，以下の (1)~(4) の条件か，Free Software Foundation 
+ *  上記著作権者は，以下の (1)〜(4) の条件か，Free Software Foundation 
  *  によって公表されている GNU General Public License の Version 2 に記
  *  述されている条件を満たす場合に限り，本ソフトウェア（本ソフトウェア
  *  を改変したものを含む．以下同じ）を使用・複製・改変・再配布（以下，
